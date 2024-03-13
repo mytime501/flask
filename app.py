008809164d40673c43e00f3a013fbbf879b591ab
mytime501 <162465951+mytime501@users.noreply.github.com>
2024-03-13 23:43:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, make_response
 from pymongo import MongoClient
-import datetime
+from datetime import timedelta, datetime, timezone
+import gridfs
+import jwt
+import hashlib
+import string
+import random
+
 app = Flask(__name__)
 app.secret_key="My_Key"
-
+SECRET_KEY = 'my_key'
 client = MongoClient(
             host="mongodb://svc.sel5.cloudtype.app",
             port=30997,
@@ -13,27 +19,56 @@ client = MongoClient(
             # authSource=auth database
         )
 db = client['who'] ## db name
-        
+
+
 @app.route("/")
 def hello():
     return render_template("hello.html")
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET','POST'])
+def home():
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.user.find_one({"id": payload['id']})
+        return render_template("home.html")
+    except jwt.ExpiredSignatureError:
+        flash("로그인 시간이 만료되었습니다.")
+        return redirect("/login")
+    except jwt.exceptions.DecodeError:
+        flash("로그인 정보가 존재하지 않습니다.")
+        return redirect("/login")
+
+@app.route("/login", methods=['GET','POST'])
 def login():
     if request.method == 'GET':
         return render_template("login.html")
     else:
-        global useridnow
-        useridnow = request.form.get("useridnow")
-        userpwnow = request.form.get("userpwnow")
-        result = db.whostudy.find_one({'userid':useridnow, 'userpw':userpwnow})
+        #global useridnow
+        useridnow = request.form["useridnow"]
+        userpwnow = request.form["userpwnow"]
+
+        userpw_hide=hashlib.sha256(userpwnow.encode('utf-8')).hexdigest()
+
+        result = db.whostudy.find_one({'userid':useridnow, 'userpw':userpw_hide})
         if result != None:
+            
+            payload = {
+            'id': useridnow,
+            'exp': datetime.now(timezone(timedelta(hours=9))) + timedelta(minutes=5)
+            }
+            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+            resp = make_response(redirect("/home"))
+            resp.set_cookie('mytoken', token)
+
             flash("{}님 로그인 되었습니다.".format(useridnow))
-            return redirect("attendance")
+            return resp
         else:
             flash("ID와 PW를 확인하세요.")
             return render_template("login.html")
-        
+            
 @app.route("/join", methods=['GET', 'POST'])
 def join_in():
     if request.method == 'GET':
@@ -65,30 +100,55 @@ def join_in():
             flash("전화번호를 확인하세요.")
             return render_template("join.html")
         else:
-            db.whostudy.insert_one({'userid': userid, 'userpw': userpw, 'username': username, 'usernumb': usernumb, 'playd': playd})
+            userpw_hide=hashlib.sha256(userpw.encode('utf-8')).hexdigest()
+            db.whostudy.insert_one({'userid': userid, 'userpw': userpw_hide, 'username': username, 'usernumb': usernumb, 'playd': playd})
             return redirect('/')
     
 @app.route("/attendance", methods=['GET', 'POST'])
 def attendance():
-    if request.method == 'GET':
-        return render_template("attendance.html")
-    else:
-        check1 = request.form.get("check1")
-        if check1 == None:
-            check1 = False
-        else:
-            check1 = True
-        print(check1)
-        if check1 == True:
-            now = datetime.datetime.now()
-            nowtime = now.strftime("%Y{} %m{} %d{} %H{} %M{} %S{}")
-            nowtime = nowtime.format('년','월','일','시','분','초')
-            db.whostudycheck.insert_one({'userid': "test", 'nowtime': nowtime})
-            flash("출석되었습니다.")
-            return redirect('/')
-        else:
-            return render_template("attendance.html")
+    token_receive = request.cookies.get('mytoken')
 
+    _LENGTH = 10
+    string_pool = string.ascii_letters + string.digits
+    security = ""
+
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.whostudy.find_one({"userid": payload['id']})
+
+        if request.method == 'GET':            
+            for i in range(_LENGTH):
+                security += random.choice(string_pool)
+
+            global securitycode
+            securitycode = security
+            return render_template("attendance.html", value = security)
+        else:
+            security_code = request.form.get("security_code")
+
+            if security_code == securitycode:
+                now = datetime.now(timezone(timedelta(hours=9)))
+
+                nowtime = now.strftime("%Y{} %m{} %d{} %H{} %M{} %S{}")
+                nowtime = nowtime.format('년','월','일','시','분','초')
+                
+                img = request.files['image']
+                
+                fs = gridfs.GridFS(db)
+                fs.put(img, filename = user_info['userid'], uploadDatenow = nowtime)
+                db.whostudycheck.insert_one({'userid': user_info['userid'], 'username': user_info['username'], 'nowtime': nowtime})
+                flash("출석되었습니다.")
+                return redirect('/home')
+            else:
+                flash("인증키가 틀렸습니다.")
+                return render_template("attendance.html", value = securitycode)
+    except jwt.ExpiredSignatureError:
+        flash("로그인 시간이 만료되었습니다.")
+        return redirect("/login")
+    except jwt.exceptions.DecodeError:
+        flash("로그인 정보가 존재하지 않습니다.")
+        return redirect("/login")
 
 if __name__ == "__main__":
     app.run('0.0.0.0',port=5000,debug=True)
